e0fa77834826c6b9120b197c8edc95d97b11f600
TGab99 <gwolf938@gmail.com>
2022-03-30 11:55:30 +0200

    Create main.py file with basic settings

---
+++ main.py
@@ -0,0 +1,19 @@
+# import the Turtle Graphics module
+import turtle
+
+# Define constants
+# Define WIDTH of the screen
+WIDTH = 500
+# Define HEIGHT of the screen
+HEIGHT = 500
+
+# Create the window
+screen = turtle.Screen()
+# Set the dimensions of the window
+screen.setup(WIDTH, HEIGHT)
+# Set the title
+screen.title("Snake Game")
+# Set th background of the window
+screen.bgcolor("darkblue")
+
+turtle.done()
