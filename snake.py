5b02420687d5979447b059fdf03238f811a345fe
TGab99 <gwolf938@gmail.com>
2022-03-31 13:04:08 +0200

    Create move_snake() function

--- snake.py
+++ snake.py
@@ -1,13 +1,15 @@
 # import Turtle module
 import turtle
 
-class Snake:
+class Snake():
 
-    def __init__(self, shape):
+    def __init__(self, shape, screen):
         self.snake = turtle.Turtle()
         self.snake.shape(shape)
         self.snake_pos = [[0, 0], [20, 0], [40, 0], [60,0]]
         self.snake.penup()
+        self.screen = screen
+        self.DELAY = 400
 
     def draw_snake(self):
         for sp in self.snake_pos:
@@ -15,4 +17,17 @@ class Snake:
             self.snake.stamp()
 
     def move_snake(self):
-        pass
+        self.snake.clearstamps()
+
+        new_head = self.snake_pos[-1].copy()
+        new_head[0] += 10
+
+        self.snake_pos.append(new_head)
+
+        self.snake_pos.pop(0)
+
+        self.draw_snake()
+
+        self.screen.update()
+
+        turtle.ontimer(self.move_snake, self.DELAY)
