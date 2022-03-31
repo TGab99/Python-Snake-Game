cac4433a9bf5083a12e3e5b5bd68265b2ad9c06d
TGab99 <gwolf938@gmail.com>
2022-03-31 11:18:57 +0200

    Import snake and food and create snake and food

--- main.py
+++ main.py
@@ -2,16 +2,16 @@
 import turtle
 # import Random module
 import random
+# import snake
+import snake
+# import food
+import food
 
 # Define constants
 # Define WIDTH of the screen
 WIDTH = 500
 # Define HEIGHT of the screen
 HEIGHT = 500
-# Define FOOD_SIZE of the food
-FOOD_SIZE = 10
-# Set snake's move in milliseconds
-DELAY = 400
 
 # Create the window
 screen = turtle.Screen()
@@ -23,29 +23,13 @@ screen.title("Snake Game")
 screen.bgcolor("darkblue")
 
 # Create the snake
-snake = turtle.Turtle()
-# Set snake's shape as square
-snake.shape("square")
-snake.penup()
+s = snake.Snake("square")
+# Draw the snake
+s.draw_snake()
 
-snake_pos = [[0, 0], [20, 0], [40, 0], [60,0]]
-
-# Draw the snake for the first time
-for sp in snake_pos:
-    snake.goto(sp[0], sp[1])
-    snake.stamp()
-
-snake.forward(100)
-
-# Create the food which the snake has to pick up
-food = turtle.Turtle()
-# Set food's shape as circle
-food.shape("circle")
-# Set food's shapesize: FOOD_SIZE / 40
-food.shapesize(FOOD_SIZE / 20)
-food.penup()
-
-food_pos = (random.randint( - WIDTH / 2 + FOOD_SIZE, WIDTH / 2 + FOOD_SIZE), random.randint( - HEIGHT / 2 + FOOD_SIZE, HEIGHT / 2 + FOOD_SIZE))
-food.goto(food_pos)
+# Create the food
+f = food.Food("circle", 10)
+# Draw the food
+f.draw_food()
 
 turtle.done()
