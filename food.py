338dabb41e45b83e5436d83449f9dda7ceb74353
TGab99 <gwolf938@gmail.com>
2022-03-31 13:33:15 +0200

    Create get_distance function and rename draw_food to food_collision

--- food.py
+++ food.py
@@ -15,11 +15,22 @@ class Food:
         self.food_pos = self.get_random_food_pos()
         self.food.penup()
 
-    def draw_food(self):
-        self.food.goto(self.food_pos)
+    def food_collision(self, snake_pos):
+        if self.get_distance(snake_pos[-1], self.food_pos) < 20:
+            self.food_pos = self.get_random_food_pos()
+            self.food.goto(self.food_pos)
+            return True
+
+        return False
 
     def get_random_food_pos(self):
         x = random.randint( - self.width / 2 + self.FOOD_SIZE, self.width / 2 - self.FOOD_SIZE)
         y = random.randint( - self.height / 2 + self.FOOD_SIZE, self.height / 2 - self.FOOD_SIZE)
 
         return (x,y)
+
+    def get_distance(self, pos1, pos2):
+        x1, y1 = pos1
+        x2, y2 = pos2
+
+        return ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5
