02c1e6b974be6c70f8f3a19cc64914f9b2c5ab14
TGab99 <gwolf938@gmail.com>
2022-03-31 13:03:47 +0200

    Rewrite main.py file and import game file

--- main.py
+++ main.py
@@ -1,35 +1,5 @@
-# import the Turtle Graphics module
-import turtle
-# import Random module
-import random
-# import snake
-import snake
-# import food
-import food
+import game
 
-# Define constants
-# Define WIDTH of the screen
-WIDTH = 500
-# Define HEIGHT of the screen
-HEIGHT = 500
-
-# Create the window
-screen = turtle.Screen()
-# Set the dimensions of the window
-screen.setup(WIDTH, HEIGHT)
-# Set the title
-screen.title("Snake Game")
-# Set the background of the window
-screen.bgcolor("darkblue")
-
-# Create the snake
-s = snake.Snake("square")
-# Draw the snake
-s.draw_snake()
-
-# Create the food
-f = food.Food("circle", 10)
-# Draw the food
-f.draw_food()
-
-turtle.done()
+if __name__ == "__main__":
+    g = game.Game()
+    g.main()
