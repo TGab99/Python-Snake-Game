f7f5aec150f7f83f9c513c8a84ad4ba1e85026d5
TGab99 <gwolf938@gmail.com>
2022-03-31 13:03:24 +0200

    Create a game.py file with class and copy the main.py's content and paset it in this

---
+++ game.py
@@ -0,0 +1,35 @@
+# import the Turtle Graphics module
+import turtle
+# import snake
+import snake
+# import food
+import food
+
+class Game():
+
+    def __init__(self):
+        # Define WIDTH of the screen
+        self.WIDTH = 500
+        # Define HEIGHT of the screen
+        self.HEIGHT = 500
+        # Create the window
+        self.screen = turtle.Screen()
+        # Set the dimensions of the window
+        self.screen.setup(self.WIDTH, self.HEIGHT)
+        # Set the title
+        self.screen.title("Snake Game")
+        # Set the background of the window
+        self.screen.bgcolor("darkblue")
+        # Create the snake
+        self.s = snake.Snake("square", self.screen)
+        # Create the food
+        self.f = food.Food("circle", 10)
+
+    def main(self):
+        # Call move_snake function
+        self.s.move_snake()
+        
+        # Draw the food
+        self.f.draw_food()
+        
+        turtle.done()
